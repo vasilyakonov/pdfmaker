ceda3d4353dc088baf17741dada3dfc09aa32073
Glitch (field-separated) <none>
2018-03-04 16:22:05 +0000

    🚟🌌 Updated with Glitch

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def create_pdf():
           # Position at 1.5 cm from bottom
           self.set_y(-15)
           # Arial italic 8
-          self.set_font('Arial', 'I', 8)
+          self.set_font('font.ttf', 'I', 8)
           # Page number
           self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
 
@@ -52,7 +52,7 @@ def create_pdf():
   pdf = PDF()
   pdf.alias_nb_pages()
   pdf.add_page()
-  pdf.set_font('Times', '', 12)
+  pdf.set_font('font.ttf', '', 12)
   for i in range(1, 41):
       pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
   pdf.output('tuto2.pdf', 'F')
