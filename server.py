5777192d8b242121135328ecfaff0769b7c98fe3
Glitch (field-separated) <none>
2018-03-04 17:08:28 +0000

    💏🎹 Updated with Glitch

--- server.py
+++ server.py
@@ -40,9 +40,7 @@ class PDF(FPDF):
         #self.cell(30, 10, 'Title', 1, 0, 'C')
         # Line break
        # self.ln(20)
-
-    # Page footer
-    def footer(self):
+      def footer(self):
         # Position at 1.5 cm from bottom
         self.set_y(-15)
         # Arial italic 8
@@ -50,7 +48,9 @@ class PDF(FPDF):
         # Page number
         self.cell(0, 10, str(self.page_no()), 0, 0, 'C')
 
-# Instantiation of inherited class
+    
+
+    # Instantiation of inherited class
 pdf = PDF()
 # Add a Unicode free font
 pdf.add_font('font', '', 'font.ttf', uni=True)
@@ -60,7 +60,29 @@ pdf.add_font('sysfont', '', r"/app/font.ttf", uni=True)
 pdf.alias_nb_pages()
 pdf.add_page()
 pdf.set_font('font', '', 18)
-pdf.cell(0, 10, "King of Carrot Flowers", 0, 1)
+pdf.cell(0, 30, "King of Carrot Flowers", 0, 1)
+class PDF(FPDF):
+    #def header(self):
+        # Logo
+        #self.image('logo_pb.png', 10, 8, 33)
+        # Arial bold 15
+        #self.set_font('Arial', 'B', 15)
+        # Move to the right
+        #self.cell(80)
+        # Title
+        #self.cell(30, 10, 'Title', 1, 0, 'C')
+        # Line break
+       # self.ln(20)
+      def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Page number
+        self.cell(0, 10, str(self.page_no()), 0, 0, 'C')
+
+
+
 pdf.add_page()
 pdf.set_font('font', '', 12)
 for i in range(1, 41):
